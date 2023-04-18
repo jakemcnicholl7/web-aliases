2c889f973ddbc74a4011edb37ee850ad60a11d5c
Jake McNicholl <jakemcnicholl@gmail.com>
2023-04-18 18:44:36 +0100

    add additional alias constructor based on a hash map

--- src/logic/alias.js
+++ src/logic/alias.js
@@ -1,6 +1,13 @@
 export default class Alias {
-    constructor(name, url){
+    constructor(name, url) {
         this.name = name;
         this.url = url;
+    }
+
+    static create(alias_values) {
+        if ("name" in alias_values && "url" in alias_values) {
+            return new Alias(alias_values["name"], alias_values["url"])
+        }
+        throw Error("ERROR: Name and url must be specifed")
     }
 }
